0f0707471494b34a54d88bba83286f4e311f2ef0
Davi Carreiro <carreirodavi3702gmail.com>
2024-11-17 16:11:23 -0300

    Segunda atualização

--- src/Candidato.java
+++ src/Candidato.java
@@ -1,5 +1,18 @@
 public class Candidato {
 
-    
+    // Criando Atributos.
+    private int idCandidato;
+    private String nomeDoCandidato;
+    private Cargo cargo;
+    private String partido;
+
+    // Construtor
+    public Candidato(int idCandidato, String nomeCandidato, Cargo cargo, String partido){
+        this.idCandidato = idCandidato;
+        this.nomeDoCandidato = nomeCandidato;
+        this.cargo = cargo;
+        this.partido = partido;
+    }
+
     
 }
