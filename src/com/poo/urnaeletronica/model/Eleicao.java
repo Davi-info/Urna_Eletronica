afbcf660cc69b51d8965b6f8e47e54d5e9c224b7
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2025-01-15 23:11:57 -0300

    Cria metodos cadastrarEleitores e listarEleitores

--- src/com/poo/urnaeletronica/model/Eleicao.java
+++ src/com/poo/urnaeletronica/model/Eleicao.java
@@ -1,34 +1,24 @@
 package com.poo.urnaeletronica.model;
 
+import com.poo.urnaeletronica.enums.TipoEleicao;
+
 import java.util.ArrayList;
-import java.util.List;
 
 public class Eleicao {
     private int id;
     private String nome;
-    private List<Candidato> candidatos;
+    private TipoEleicao tipoEleicao;
+    private ArrayList<Candidato> candidatos;
+    private ArrayList<Eleitor> eleitores;
 
-    public Eleicao(int id, String nome) {
-        this.setId(id);
-        this.setNome(nome);
+    public Eleicao(int id, String nome, TipoEleicao tipoEleicao) {
+        this.id = id;
+        this.nome = nome;
+        this.tipoEleicao = tipoEleicao;
         this.candidatos = new ArrayList<>();
-    }
+		this.eleitores = new ArrayList<>();
+	}
 
-    public void cadastrarCandidato(Candidato c) {
-        candidatos.add(c);
-    }
-
-    public List<Candidato> getCandidatos() {
-        return candidatos;
-    }
-
-    // Método para listar os candidatos
-    public void listarCandidatos() {
-        System.out.println("Candidatos disponíveis:");
-        for (Candidato c : candidatos) {
-            System.out.println("ID: " + c.getId() + " - Nome: " + c.getNome() + " - Partido: " + c.getPartido());
-        }
-    }
 
     // Getters
 	public String getNome() {
@@ -47,4 +37,35 @@ public class Eleicao {
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	public void cadastrarCandidato(Candidato c) {
+        candidatos.add(c);
+    }
+
+    public ArrayList<Candidato> getCandidatos() {
+        return candidatos;
+    }
+
+	public void cadastrarEleitor(Eleitor e) {
+        eleitores.add(e);
+    }
+
+    public ArrayList<Eleitor> getEleitores() {
+        return eleitores;
+    }
+
+    // Método para listar os candidatos
+    public void listarCandidatos() {
+        System.out.println("Candidatos disponíveis:");
+        for (Candidato c : candidatos) {
+            System.out.println("ID: " + c.getId() + " - Nome: " + c.getNome() + " - Partido: " + c.getPartido());
+        }
+
+    } // Método para listar os eleitores
+    public void listarEleitores() {
+        System.out.println("Eleitores cadastrados");
+        for (Eleitor e : eleitores) {
+            System.out.println(" Nome: " + e.getNome());
+        }
+    }
 }
