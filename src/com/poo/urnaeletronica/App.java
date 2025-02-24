e2a7907ea6014a916f59b891a3fdbf35945b672a
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2025-02-24 00:37:00 -0300

    Ajeita formatacao

--- src/com/poo/urnaeletronica/App.java
+++ src/com/poo/urnaeletronica/App.java
@@ -2,13 +2,10 @@ package com.poo.urnaeletronica;
 
 import com.poo.urnaeletronica.view.LoginView;
 
-
-
 public class App {
     public static void main(String[] args) throws Exception {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-
                 new LoginView();
             }
         });
