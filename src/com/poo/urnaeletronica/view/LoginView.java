016bad39e56cb6f33ae197845fb3454e9cfba755
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2025-02-23 22:44:19 -0300

    Refatora LoginView parte II

--- src/com/poo/urnaeletronica/view/LoginView.java
+++ src/com/poo/urnaeletronica/view/LoginView.java
@@ -12,8 +12,7 @@ import com.poo.urnaeletronica.components.Label;
 import com.poo.urnaeletronica.controller.UsuarioController;
 import com.poo.urnaeletronica.model.Usuario;
 
-public class LoginView {
-    private JFrame frame;
+public class LoginView extends JFrame {
     private JTextField loginField;
     private JPasswordField senhaField;
     private UsuarioController usuarioController;
@@ -25,14 +24,14 @@ public class LoginView {
     }
 
     private void initialize() {
-        frame = new JFrame("Login");
-        frame.setBounds(100, 100, 683, 384);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setLayout(null);
+        setTitle("Login");
+        setBounds(100, 100, 683, 384);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
                 
         Button btnSair = new Button("Sair", 516, 291,
             100, 30, e -> System.exit(0));
-        frame.getContentPane().add(btnSair);
+        getContentPane().add(btnSair);
 
         Button btnLogin = new Button("Entrar", 378, 291,
         100, 30, e -> {
@@ -42,37 +41,37 @@ public class LoginView {
             usuarioLogado = usuarioController.autenticar(login, senha);
 
             if (usuarioLogado != null) {
-                JOptionPane.showMessageDialog(frame, "Login bem-sucedido! Bem-vindo, " + usuarioLogado.getCargo());
-                frame.dispose(); // Fecha a tela de login
+                JOptionPane.showMessageDialog(this, "Login bem-sucedido! Bem-vindo, " + usuarioLogado.getCargo());
+                dispose(); // Fecha a tela de login
                 abrirMenuPrincipal();
             } else {
-                JOptionPane.showMessageDialog(frame, "Login ou senha incorretos!", "Erro", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(this, "Login ou senha incorretos!", "Erro", JOptionPane.ERROR_MESSAGE);
             }
         });
-        frame.getContentPane().add(btnLogin);
+        getContentPane().add(btnLogin);
 
         Label lblLogin = new Label("Login:", 374, 93, 80, 25);
-        frame.getContentPane().add(lblLogin);
+        getContentPane().add(lblLogin);
 
         loginField = new JTextField();
         loginField.setBounds(374, 129, 242, 25);
-        frame.getContentPane().add(loginField);
+        getContentPane().add(loginField);
         loginField.setColumns(10);
 
         Label lblSenha = new Label("Senha:", 374, 175, 80, 25);
-        frame.getContentPane().add(lblSenha);
+        getContentPane().add(lblSenha);
 
         senhaField = new JPasswordField();
         senhaField.setBounds(374, 211, 242, 25);
-        frame.getContentPane().add(senhaField);
+        getContentPane().add(senhaField);
         
         // Imagem de fundo
         JLabel lblNewLabel = new JLabel("New label");
         lblNewLabel.setIcon(new ImageIcon(LoginView.class.getResource("../images/TelaLogin.png")));
         lblNewLabel.setBounds(0, 0, 667, 345);
-        frame.getContentPane().add(lblNewLabel);
+        getContentPane().add(lblNewLabel);
 
-        frame.setVisible(true);
+        setVisible(true);
     }
 
     private void abrirMenuPrincipal() {
