2f655460343c94275794b2b7b27cf7a0de718038
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2025-02-24 00:30:23 -0300

    Refatora Menu

--- src/com/poo/urnaeletronica/view/Menu.java
+++ src/com/poo/urnaeletronica/view/Menu.java
@@ -1,131 +1,73 @@
 package com.poo.urnaeletronica.view;
 
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import com.poo.urnaeletronica.components.Botao;
+
 
 public class Menu extends JFrame{
-
-	//private JFrame frame;
-
-	/**
-	 * Create the application.
-	 */
 	public Menu() {
 		initialize();
 	}
 
-	
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize() {
-		//frame = new JFrame();
 		setTitle("Tela de Menu");
 		setBounds(100, 100, 1025, 576);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(null);
 		
-		JButton btnRelatorio = new JButton("Relatório");
-		btnRelatorio.setFont(new Font("Tahoma", Font.BOLD, 15));
+		Botao btnRelatorio = new Botao("Relatório", 703, 69, 147, 79, e -> {});
 		btnRelatorio.setIcon(new ImageIcon(Menu.class.getResource("../images/IconeRelatorio.png")));
-		btnRelatorio.setBounds(703, 69, 147, 79);
 		getContentPane().add(btnRelatorio);
-		btnRelatorio.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
 		
-		JButton btnSair = new JButton("Sair");
-		btnSair.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btnSair.setBounds(637, 406, 125, 79);
+		Botao btnSair = new Botao("Sair", 637, 406, 125, 79, e -> System.exit(0));
 		getContentPane().add(btnSair);
-		btnSair.addActionListener(e -> System.exit(0));
 		
-		JButton btnLimpar = new JButton("Limpar Dados do Banco");
+		Botao btnLimpar = new Botao("Limpar Dados do Banco", 530, 183, 324, 79, e -> {});
 		btnLimpar.setIcon(new ImageIcon(Menu.class.getResource("../images/IconeLimpaDados.png")));
-		btnLimpar.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnLimpar.setBounds(530, 183, 324, 79);
 		getContentPane().add(btnLimpar);
 		
-		JButton btnCadastrarCargo = new JButton("Cadastrar Cargo");
-		btnCadastrarCargo.setIcon(new ImageIcon(Menu.class.getResource("../images/IconeCadastro.png")));
-		btnCadastrarCargo.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnCadastrarCargo.setBounds(88, 406, 324, 79);
-		getContentPane().add(btnCadastrarCargo);
-
-		btnCadastrarCargo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				CadastrarCargo cadastrarCargo = new CadastrarCargo(); // Cria a tela de cadastro de cargo
-				cadastrarCargo.setVisible(true); // Torna a tela visível
-			}
+		Botao btnCadastrarCargo = new Botao("Cadastrar Cargo", 88, 406, 324, 79, e -> {
+			CadastrarCargo cadastrarCargo = new CadastrarCargo(); // Cria a tela de cadastro de cargo
+			cadastrarCargo.setVisible(true); // Torna a tela visível
 		});
-
+		btnCadastrarCargo.setIcon(new ImageIcon(Menu.class.getResource("../images/IconeCadastro.png")));
+		getContentPane().add(btnCadastrarCargo);
 		
-		JButton btnCadastrarEleicao = new JButton("Cadastrar Eleição");
+		Botao btnCadastrarEleicao = new Botao("Cadastrar Eleição", 88, 294, 324, 79, e -> {
+			CadastrarEleicao cadastrarEleicao = new CadastrarEleicao(); // Cria a tela de cadastro de cargo
+			cadastrarEleicao.setVisible(true); // Torna a tela visível
+		});
 		btnCadastrarEleicao.setIcon(new ImageIcon(Menu.class.getResource("../images/IconeCadastro.png")));
-		btnCadastrarEleicao.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnCadastrarEleicao.setBounds(88, 294, 324, 79);
 		getContentPane().add(btnCadastrarEleicao);
 
-		btnCadastrarEleicao.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				CadastrarEleicao cadastrarEleicao = new CadastrarEleicao(); // Cria a tela de cadastro de cargo
-				cadastrarEleicao.setVisible(true); // Torna a tela visível
-			}
-		});
-		
-		JButton btnCadastrarCandidato = new JButton("Cadastrar Candidato");
-		btnCadastrarCandidato.setIcon(new ImageIcon(Menu.class.getResource("../images/IconeCadastro.png")));
-		btnCadastrarCandidato.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnCadastrarCandidato.setBounds(88, 183, 324, 79);
-		getContentPane().add(btnCadastrarCandidato);
-
-		btnCadastrarCandidato.addActionListener(new ActionListener() {	
-		public void actionPerformed(ActionEvent e) {
+		Botao btnCadastrarCandidato = new Botao("Cadastrar Candidato", 88, 183, 324, 79, e -> {
 			CadastrarCandidato cadastrarCandidato = new CadastrarCandidato(); // Cria a tela de cadastro de cargo
 			cadastrarCandidato.setVisible(true); // Torna a tela visível
-			}
-		});
-
+			});
+		btnCadastrarCandidato.setIcon(new ImageIcon(Menu.class.getResource("../images/IconeCadastro.png")));
+		getContentPane().add(btnCadastrarCandidato);
 		
-		JButton btnCadastrarEleitor = new JButton("Cadastrar Eleitor");
+		Botao btnCadastrarEleitor = new Botao("Cadastrar Eleitor", 88, 69, 324, 79, e -> {
+			CadastrarEleitor cadastrarEleitor = new CadastrarEleitor(); // Cria a tela de cadastro de cargo
+			cadastrarEleitor.setVisible(true); // Torna a tela visível
+		});
 		btnCadastrarEleitor.setIcon(new ImageIcon(Menu.class.getResource("../images/IconeCadastro.png")));
-		btnCadastrarEleitor.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnCadastrarEleitor.setBounds(88, 69, 324, 79);
 		getContentPane().add(btnCadastrarEleitor);
 
-		btnCadastrarEleitor.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				CadastrarEleitor cadastrarEleitor = new CadastrarEleitor(); // Cria a tela de cadastro de cargo
-				cadastrarEleitor.setVisible(true); // Torna a tela visível
-			}
+		Botao btnUrnaEletronica = new Botao("Urna", 530, 69, 147, 79, e -> {
+			UrnaEletronica urnaEletronica = new UrnaEletronica(); // Cria a tela de cadastro de cargo
+			urnaEletronica.setVisible(true); // Torna a tela visível
 		});
-		
-		JButton btnUrnaEletronica = new JButton("Urna");
-		btnUrnaEletronica.setFont(new Font("Tahoma", Font.BOLD, 15));
 		btnUrnaEletronica.setIcon(new ImageIcon(Menu.class.getResource("../images/IconeUrna.png")));
-		btnUrnaEletronica.setBounds(530, 69, 147, 79);
 		getContentPane().add(btnUrnaEletronica);
-
-		btnUrnaEletronica.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				UrnaEletronica urnaEletronica = new UrnaEletronica(); // Cria a tela de cadastro de cargo
-				urnaEletronica.setVisible(true); // Torna a tela visível
-			}
-		});
 		
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(0, 0, 1009, 537);
-		lblNewLabel.setIcon(new ImageIcon(Menu.class.getResource("../images/TelaMenu.png")));
-		getContentPane().add(lblNewLabel);
+		JLabel imagemDeFundo = new JLabel("Imagem de fundo");
+		imagemDeFundo.setBounds(0, 0, 1009, 537);
+		imagemDeFundo.setIcon(new ImageIcon(Menu.class.getResource("../images/TelaMenu.png")));
+		getContentPane().add(imagemDeFundo);
 	}
 
 		
